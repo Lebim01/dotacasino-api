b1319e81a1faf779b37a5052f7e6f80411476715
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-08 14:12:29 -0600

    kyc

--- apps/client-api/src/kyc/dto/submit-kyc.dto.ts
+++ apps/client-api/src/kyc/dto/submit-kyc.dto.ts
@@ -2,6 +2,26 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsString, ArrayNotEmpty } from 'class-validator';
 
 export class SubmitKycDto {
+  @ApiProperty()
+  @IsString()
+  name!: string;
+
+  @ApiProperty()
+  @IsString()
+  last_name!: string;
+
+  @ApiProperty()
+  @IsString()
+  id!: string;
+
+  @ApiProperty()
+  @IsString()
+  country!: string;
+
+  @ApiProperty()
+  @IsString()
+  birthday!: string;
+
   @ApiProperty({
     description: 'Array of document IDs to submit for verification',
     example: ['123e4567-e89b-12d3-a456-426614174000']
