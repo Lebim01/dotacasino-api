c110add210c5c34be627dfc3c50f5abac6c51281
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-26 14:45:46 -0600

    fix

--- apps/client-api/src/memberships/memberships.module.ts
+++ apps/client-api/src/memberships/memberships.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { MembershipsController } from './memberships.controller';
 import { MembershipsService } from './memberships.service';
 import { UserCommonService } from '@domain/users/users.service';
+import { AuthAcademyService } from '@domain/auth-academy/auth-academy.service';
+import { DisruptiveService } from '@domain/disruptive/disruptive.service';
+import { CasinoService } from '@domain/casino/casino.service';
 
 @Module({
   controllers: [MembershipsController],
-  providers: [MembershipsService, UserCommonService],
+  providers: [
+    MembershipsService,
+    UserCommonService,
+    AuthAcademyService,
+    DisruptiveService,
+    CasinoService,
+  ],
 })
 export class MembershipsModule {}
