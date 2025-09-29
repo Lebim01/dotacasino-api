afe8adceb53080f7ff1a721018025bca452227e0
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-29 09:38:40 -0700

    log

--- apps/admin-api/src/health.controller.ts
+++ apps/admin-api/src/health.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post } from '@nestjs/common';
 
 @Controller()
 export class HealthController {
-  
   @Get('/')
   ping() {
     return { ok: true, app: 'admin-api' };
+  }
+
+  @Post('log')
+  log(@Body() body: any, @Headers() headers: any) {
+    console.log(headers);
+    console.log(body);
   }
 }
