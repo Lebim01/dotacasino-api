6e4873497d3fcdd2acf55c9eba5e4f69f3629466
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-30 11:47:34 -0700

    backoffice v1

--- apps/backoffice-api/src/main.ts
+++ apps/backoffice-api/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   });
 
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('v1');
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
