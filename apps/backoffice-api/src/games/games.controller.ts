76e1d7896a113e861b9e8884e5826751e8bc9473
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-10-22 15:28:06 -0300

    game list

--- apps/backoffice-api/src/games/games.controller.ts
+++ apps/backoffice-api/src/games/games.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { GamesService } from './games.service';
+import { ListGamesDto } from './dto/list-games.dto';
 
 @Controller('games')
 export class GamesController {
   constructor(private readonly gamesService: GamesService) {}
 
   @Get('admin-list')
-  async adminlist() {
-    return this.gamesService.adminlist();
+  async adminlist(@Query() q: ListGamesDto) {
+    return this.gamesService.list(q);
+  }
+
+  @Post('change-show/:id')
+  async changeshow(@Param('id') id: string, @Body() body) {
+    return this.gamesService.changeshow(id, body.show);
   }
 }
--- apps/backoffice-api/src/games/games.service.ts
+++ apps/backoffice-api/src/games/games.service.ts
@@ -1,17 +1,62 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'libs/db/src/prisma.service';
+import { ListGamesDto } from './dto/list-games.dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class GamesService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async adminlist() {
-    const games = await this.prisma.game.findMany({
-      orderBy: {
-        createdAt: 'asc',
+  async list(q: ListGamesDto) {
+    const where: Prisma.GameWhereInput = { enabled: true };
+
+    if (q.search) {
+      where.OR = [
+        { title: { contains: q.search, mode: 'insensitive' } },
+        { slug: { contains: q.search, mode: 'insensitive' } },
+        { gameType: { contains: q.search, mode: 'insensitive' } },
+      ];
+    }
+
+    const skip = (q.page - 1) * q.pageSize;
+    const [items, total] = await this.prisma.$transaction([
+      this.prisma.game.findMany({
+        where,
+        orderBy: {
+          createdAt: 'asc',
+        },
+        skip,
+        take: q.pageSize,
+        select: {
+          id: true,
+          slug: true,
+          title: true,
+          category: true,
+          devices: true,
+          thumbnailUrl: true,
+          show: true,
+          GameProvider: true,
+        },
+      }),
+      this.prisma.game.count({ where }),
+    ]);
+
+    return {
+      page: q.page,
+      pageSize: q.pageSize,
+      total,
+      items,
+    };
+  }
+
+  async changeshow(id: string, show: boolean) {
+    return this.prisma.game.update({
+      where: {
+        id,
+      },
+      data: {
+        show,
       },
     });
-    
-    return games;
   }
 }
