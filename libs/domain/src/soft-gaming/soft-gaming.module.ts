f7524f7b873d951f5e8c88c9841da7ba252159e6
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2026-01-07 11:56:03 -0700

    list

--- libs/domain/src/soft-gaming/soft-gaming.module.ts
+++ libs/domain/src/soft-gaming/soft-gaming.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'libs/db/src/prisma.service';
 import { SoftGamingController } from './soft-gaming.controller';
 import { SoftGamingService } from './soft-gaming.service';
-import { HttpService } from '@nestjs/axios';
 
 @Module({
   providers: [PrismaService, SoftGamingService],
