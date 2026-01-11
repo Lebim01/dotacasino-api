0763e2f4a0bab4e02d94df2971a076cbc06fb1b5
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2026-01-11 09:36:27 -0700

    opengame

--- libs/domain/src/soft-gaming/soft-gaming.controller.ts
+++ libs/domain/src/soft-gaming/soft-gaming.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Ip, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '@security/jwt.guard';
+import { CurrentUser } from '@security/current-user.decorator';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { SoftGamingService } from './soft-gaming.service';
 
+@ApiTags('SoftGaming')
 @Controller('soft-gaming')
 export class SoftGamingController {
-  constructor(private readonly softGamingService: SoftGamingService) {}
+  constructor(private readonly softGamingService: SoftGamingService) { }
 
   @Get('game-list')
   gamelist() {
@@ -18,5 +22,21 @@ export class SoftGamingController {
   @Get('sync-categories')
   syncCategories() {
     return this.softGamingService.syncCategories();
+  }
+
+  @Post('open-game')
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
+  openGame(
+    @Body('gameId') gameId: string,
+    @CurrentUser() user: { userId: string },
+    @Ip() ip: string,
+  ) {
+    console.log({
+      gameId,
+      user,
+      ip
+    })
+    return this.softGamingService.getAuthorizationUser(user.userId, gameId, ip);
   }
 }
